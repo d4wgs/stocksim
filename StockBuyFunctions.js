14802b81f23b5e4ebf685a2480e11aaab0e1c26f
Steve K <68475815+d4wgs@users.noreply.github.com>
2023-11-20 19:54:12 -0500

    Update StockBuyFunctions.js

--- StockBuyFunctions.js
+++ StockBuyFunctions.js
@@ -24,3 +24,30 @@ function buyTSLA() {
       document.getElementById("TSLAqty").innerHTML = TSLAqty;
     }
   }
+
+  function sellAMZ() {
+    if (AMZqty > 0) {
+      Cash = Cash + AMZprice;
+      AMZqty = AMZqty - 1;
+      document.getElementById("Cash").innerHTML = Cash;
+      document.getElementById("AMZqty").innerHTML = AMZqty;
+    }
+  }
+
+  function sellAAPL() {
+    if (AAPLqty > 0) {
+      Cash = Cash + AAPLprice;
+      AAPLqty = AAPLqty - 1;
+      document.getElementById("Cash").innerHTML = Cash;
+      document.getElementById("AAPLqty").innerHTML = AAPLqty;
+    }
+  }
+
+  function sellTSLA() {
+    if (TSLAqty > 0) {
+      Cash = Cash + TSLAprice;
+      TSLAqty = TSLAqty - 1;
+      document.getElementById("Cash").innerHTML = Cash;
+      document.getElementById("TSLAqty").innerHTML = TSLAqty;
+    }
+  }
